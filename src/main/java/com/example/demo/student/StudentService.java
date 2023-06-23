1ebe0c299128f97dfd623b9b5a6c03206c9839da
MaxenceAll <maxence.allart@gmail.com>
2023-06-23 16:09:54 +0200

    Post request done

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -17,15 +19,36 @@ public class StudentService {
         return studentRepository.findAll();
     }
     public void addNewStudent(Student student) {
-
-        System.out.println(student);
-
         Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
         if (studentOptional.isPresent()){
             throw new IllegalStateException("Email taken");
         } else{
             studentRepository.save(student);
         }
+    }
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (exists){
+            studentRepository.deleteById(studentId);
+        } else {
+            throw new IllegalStateException("Student with id ("+ studentId + ") doesnt exists !");
+        }
+    }
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
 
+        Student student = studentRepository
+                .findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id ("+ studentId + ") doesn't exist!"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("Email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
